db1e0bd94330a431caf0b5121cf239b3a8514880
Enrique Albarrán <123507833+Ealbarran200510@users.noreply.github.com>
2024-08-28 22:18:55 -0600

    Update

--- Encriptador de texto/js/script.js
+++ Encriptador de texto/js/script.js
@@ -1,32 +1,53 @@
-const msj = document.querySelector(".inputText");
-const encMsj = document.querySelector(".txtEnc");
+/* Mediante el uso del DOM se recupera el mensaje ingresado por el usuario dentro del
+input mediante querySelector con la clase inputAreaMsj.
+Además se selecciona el input con la clase inputAreaEnc para mostrar la respuesta con el
+mensaje una vez encriptado*/
+
+const msjUser = document.querySelector(".inputAreaMsj");
+const encMsj = document.querySelector(".inputAreaEnc");
+
+/* Mediante btnEnc se implementa la funcionalidad del botón Encriptar. 
+La constante msjEnc llama a la función encriptar con el valor del mensaje definido por msjUser.
+Ahora el valor de encMsj que se mostrará en pantalla será igual al valor msjEnc una vez 
+que se ha ejecutado la función encriptar.
+Posteriormente el valor del input msjUser será igual a un campo vacio para limpiar 
+automáticamente el campo en pantalla después de haber dado click al botón Encriptar Mensaje.
+El botón adquiere la funcionalidad mediante el método onclick en HTML */
 
 function btnEnc(){
 
-    const msjEnc = encript(msj.value)
+    const msjEnc = encript(msjUser.value)
     encMsj.value = msjEnc
-    msj.value=""
+    msjUser.value=""
 
 }
 
-function encript (textEnc) {
+/* La función encriptar define un array que contiene todas las vocales a sustituir en el 
+mensaje.
+Recibe como parámetro el texto encriptado y lo transforma a minúsculas con toLowerCase.
+El ciclo for recorre el arreglo para sustituir cada una de las vocales con la clave
+correspondiente. Si el texto contiene algún valor del arreglo se sustituye utilizando
+replaceAll.
+Finalmente la función devuelve el texto encriptado una vez finalizado el ciclo for */
+
+function encript (txtEnc) {
     let keyMsj = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
-    textEnc = textEnc.toLowerCase()
+    txtEnc = txtEnc.toLowerCase()
 
     for ( let i = 0; i < keyMsj.length; i++){
-        if(textEnc.includes(keyMsj[i][0])){
-            textEnc = textEnc.replaceAll(keyMsj[i][0], keyMsj[i][1])
+        if(txtEnc.includes(keyMsj[i][0])){
+            txtEnc = txtEnc.replaceAll(keyMsj[i][0], keyMsj[i][1])
         }
     }
 
-    return textEnc
+    return txtEnc
 }
 
 function btnDenc(){
 
-    const msjEnc = dencript(msj.value)
-    encMsj.value = msjEnc
-    msj.value=""
+    const msjDenc = dencript(msjUser.value)
+    encMsj.value = msjDenc
+    msjUser.value=""
 
 }
 
@@ -41,4 +62,12 @@ function dencript (textDenc) {
     }
 
     return textDenc
+}
+
+function btnCopy(){
+
+    const msjCopy = encMsj.value
+    encMsj.value=msjCopy
+
+
 }
