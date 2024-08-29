0451d3be65a449699e9b699b3f6f8597393382a2
Enrique Albarrán <123507833+Ealbarran200510@users.noreply.github.com>
2024-08-28 22:24:58 -0600

    Update

--- Encriptador de texto/js/script.js
+++ Encriptador de texto/js/script.js
@@ -6,7 +6,7 @@ mensaje una vez encriptado*/
 const msjUser = document.querySelector(".inputAreaMsj");
 const encMsj = document.querySelector(".inputAreaEnc");
 
-/* Mediante btnEnc se implementa la funcionalidad del botón Encriptar. 
+/* Mediante btnEnc se implementa la funcionalidad del botón Encriptar Mensaje. 
 La constante msjEnc llama a la función encriptar con el valor del mensaje definido por msjUser.
 Ahora el valor de encMsj que se mostrará en pantalla será igual al valor msjEnc una vez 
 que se ha ejecutado la función encriptar.
@@ -24,7 +24,7 @@ function btnEnc(){
 
 /* La función encriptar define un array que contiene todas las vocales a sustituir en el 
 mensaje.
-Recibe como parámetro el texto encriptado y lo transforma a minúsculas con toLowerCase.
+Recibe como parámetro el texto desencriptado y lo transforma a minúsculas con toLowerCase.
 El ciclo for recorre el arreglo para sustituir cada una de las vocales con la clave
 correspondiente. Si el texto contiene algún valor del arreglo se sustituye utilizando
 replaceAll.
@@ -43,6 +43,14 @@ function encript (txtEnc) {
     return txtEnc
 }
 
+/* Mediante btnDenc se implementa la funcionalidad del botón Desencriptar Mensaje. 
+La constante msjDenc llama a la función desencriptar con el valor del mensaje definido por msjUser.
+Ahora el valor de encMsj que se mostrará en pantalla será igual al valor msjDenc una vez 
+que se ha ejecutado la función desencriptar.
+Posteriormente el valor del input encMsj será igual a un campo vacio para limpiar 
+automáticamente el campo en pantalla después de haber dado click al botón Desencriptar Mensaje.
+El botón adquiere la funcionalidad mediante el método onclick en HTML */
+
 function btnDenc(){
 
     const msjDenc = dencript(msjUser.value)
@@ -50,6 +58,14 @@ function btnDenc(){
     msjUser.value=""
 
 }
+
+/* La función desencriptar define el mismo array de la función encriptar
+Recibe como parámetro el texto encriptado y lo transforma a minúsculas con toLowerCase.
+El ciclo for recorre el arreglo para sustituir cada una de las claves con las vocales
+correspondientes. El recorrido de for es inverso al de la función encriptar.
+Si el texto contiene algún valor encriptado del arreglo se sustituye por la vocal
+correspondiente.
+Finalmente la función devuelve el texto desencriptado una vez finalizado el ciclo for */
 
 function dencript (textDenc) {
     let keyMsj = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
@@ -63,6 +79,12 @@ function dencript (textDenc) {
 
     return textDenc
 }
+
+// TODO
+
+/* Mediante btnCopy se implementa la funcionalidad del botón Copiar Mensaje. 
+La constante msjCopy será igual al valor contenido en el input encMsj
+El botón adquiere la funcionalidad mediante el método onclick en HTML */
 
 function btnCopy(){
 
